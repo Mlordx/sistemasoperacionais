17b029154fd5b6b26524d3d6bff456811751ea24
Vinicius <vinicius.nas.silva@gmail.com>
2015-09-21 15:38:34 -0300

    Reorganizando pastas

---
+++ ep1/rodar.pl
@@ -0,0 +1,11 @@
+for($i = 0; $i < 30; $i++){
+  `./bin/ep1 6 teste/teste_20_$i.txt teste/out/teste_20_6_$i.out.csv d`
+}
+
+for($i = 0; $i < 30; $i++){
+  `./bin/ep1 6 teste/teste_60_$i.txt teste/out/teste_60_6_$i.out.csv d`
+}
+
+for($i = 0; $i < 30; $i++){
+  `./bin/ep1 6 teste/teste_100_$i.txt teste/out/teste_100_6_$i.out.csv d`
+}
--- rodar.pl
+++
@@ -1,3 +0,0 @@
-for($i = 0; $i < 10; $i++){
-  `./bin/ep1 2 teste/teste_100_$i.txt teste/out/teste_100_2_$i.out.csv d`
-}
