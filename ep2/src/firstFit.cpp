23624db2bc2787eed8f1572b0c7d04ebeeda9ecc
Mlordx <matbarrosrodrigues@gmail.com>
2015-10-09 02:03:34 -0300

    Tirando o firstFit velho do git

--- ep2/src/firstFit.cpp
+++ ep2/src/firstFit.cpp
@@ -3,31 +3,6 @@
 #include <jobFactory.hpp>
 
 using namespace std;
-/*
-int main(){
-  cout << "teste\n";
-  JobFactory factory(40);
-  auto jobs = factory.createManyJobsRandomly(5, 20, "processo_", 50, 10, 40);
-  Memory mem("teste.mem", 1000);
-  auto head = mem.getMemoryState();
-  auto table = head;
-  FirstFit algorithm(head);
-
-  int i = 0;
-  for(;i < 40; i++){
-    cout << algorithm.execute(jobs[i]) << endl;
-    while(table != nullptr){
-      cout << "(" << table->position << ", " << table->size << ", " << table->pid << ") ";
-      table = table->next;
-    }
-    cout << endl << endl;
-    table = head;
-    mem.setMemoryState(head);
-  }
-  
-  return 0;
-}
-*/
 FirstFit::FirstFit(shared_ptr<MemorySlot> head) : MemoryAlgorithm(head),list(head) {}
 
 
