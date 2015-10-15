b54a69033837fcfffd144d9c032f1e26356b9909
Vinícius Nascimento Silva <vinicius.nascimento.silva@usp.br>
2015-10-15 14:38:16 -0300

    First fit funcionando

--- ep2/src/nextFit.cpp
+++ ep2/src/nextFit.cpp
@@ -4,33 +4,7 @@
 
 using namespace std;
 
-/*
-int main(){
-  cout << "teste\n";
-  JobFactory factory(50);
-  auto jobs = factory.createManyJobsRandomly(5, 20, "processo_", 50, 10, 40);
-  Memory mem("teste.mem", 1024);
-  auto head = mem.getMemoryState();
-  auto table = head;
-  NextFit algorithm(head);
-
-  for(int i = 0; i < 40; i++){
-    cout << algorithm.execute(jobs[i]) << endl;
-    while(table != nullptr){
-      cout << "(" << table->position << ", " << table->size << ", " << table->pid << ") ";
-      table = table->next;
-    }
-    cout << endl << endl << endl;
-    table = head;
-    mem.setMemoryState(head);
-  }
-  
-  return 0;
-}
-*/
-
 NextFit::NextFit(shared_ptr<MemorySlot> head) : MemoryAlgorithm(head), next_(head) {}
-
 
 int NextFit::execute(Job job){
   auto memoryTable = next_;
