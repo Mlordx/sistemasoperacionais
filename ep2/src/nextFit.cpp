304b74462ccf99a1958149046c99aba418fd35af
Vinícius Nascimento Silva <vinicius.nascimento.silva@usp.br>
2015-10-15 22:13:10 -0300

    Resolvido um problema de ponteiros

--- ep2/src/nextFit.cpp
+++ ep2/src/nextFit.cpp
@@ -7,8 +7,11 @@ using namespace std;
 NextFit::NextFit(shared_ptr<MemorySlot> head) : MemoryAlgorithm(head), next_(head) {}
 
 int NextFit::execute(Job job){
+  if (next_->next == nullptr)
+    next_ = head_;
   auto memoryTable = next_;
   do{
+    cout << "memoryTable->position: " << memoryTable->position << endl; 
     if(memoryTable->pid == -1 && memoryTable->size >= getRealSize(job)){
       next_ = insertJob(job, memoryTable);
       return next_->position;
